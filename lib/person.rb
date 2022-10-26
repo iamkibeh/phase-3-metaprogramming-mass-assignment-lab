692373754de2bcd8ec6e935d31d0a0e0e0300439
iamkibeh <kibet.immanuel@student.moringaschool.com>
2022-10-27 00:58:28 +0300

    complete assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,12 @@
 class Person
   # your code here
+
+  def initialize(arguments)
+    arguments.each do |key,value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=",value)
+    end
+
+  end
 end
+
